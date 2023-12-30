c922a559061f1cbb125f35c9935e3622d987e332
Victor-Eduardo <victorelalves@gmail.com>
2023-12-30 02:36:54 -0300

    Design System em React - Documentação de tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import { StoryObj, Meta } from '@storybook/react'
 import { Button, type ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'form/Button',
   component: Button,
   args: {
     children: 'Enviar',
