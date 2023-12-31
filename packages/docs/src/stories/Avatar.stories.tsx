f774dd33c52b4110529bbd5da46ca5db1eceb930
Victor-Eduardo <victorelalves@gmail.com>
2023-12-31 19:40:41 -0300

    Componentes do Design System - Componente: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/victor-com-code.png',
+    alt: 'Victor Eduardo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
