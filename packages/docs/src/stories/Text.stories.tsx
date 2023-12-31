9ece9d06b757a42015264fdbb89a436a83ad5561
Victor-Eduardo <victorelalves@gmail.com>
2023-12-31 18:58:58 -0300

    Componentes do Design System - Componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facere sapiente aspernatur quis ex voluptates fuga unde placeat, dolor, at corrupti quidem? In dolore dolorem expedita! Libero ullam et aliquam!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,5 +1,6 @@
-import { ComponentProps } from 'react'
+import { ComponentProps } from '@stitches/react'
 import { styled } from '../styles'
+import { ElementType } from 'react'
 
 export const Box = styled('div', {
   padding: '$4',
@@ -8,4 +9,6 @@ export const Box = styled('div', {
   border: '1px solid $gray',
 })
 
-export type BoxProps = ComponentProps<typeof Box>
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
